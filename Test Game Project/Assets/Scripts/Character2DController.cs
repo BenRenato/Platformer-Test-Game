7a554848813d8653f196dea9e210d3fe391e1a5a
Ben <BenRenato@users.noreply.github.com>
2021-07-10 02:52:25 +0100

    Added player, movement, run animation, score, coins
    Player:
    Now has run animation, faces correct direction when running, anchored sprite correctly.
    
    Score:
    Fixed canvas default size to match camera bounds, updates upon execution of PickUpCoin script.
    
    Coins:
    Game objects that trigger via OnEnterTrigger2D() and update score, then destroy themselves.
    
    TODO:
    Enemies and melee combat (sword slash).

--- Test Game Project/Assets/Scripts/Character2DController.cs
+++ Test Game Project/Assets/Scripts/Character2DController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Character2DController : MonoBehaviour
 {
 
+    public Animator animator;
+
     public float MovementSpeed = 1;
     public float JumpForce = 1;
     private Rigidbody2D _rigidbody;
+
+    private bool facingRight;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        facingRight = true;
     }
 
     // Update is called once per frame
@@ -22,9 +27,25 @@ public class Character2DController : MonoBehaviour
 
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f){
+        Flip(movement);
 
+        animator.SetFloat("MovementSpeed", Mathf.Abs(movement));
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
     }
+
+    private void Flip(float moveHorizontal)
+    {
+        if (moveHorizontal > 0 && !facingRight || moveHorizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
 }
